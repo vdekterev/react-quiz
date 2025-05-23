4f89bab65651fb56d88bbcd1a224eaa35d76bd38
vdekterev <vlad.decterev@icloud.com>
2025-05-23 20:59:51 +0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import DateCounter from "./components/DateCounter";
+
 function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
+    return (
+     <div>
+         <DateCounter/>
+     </div>
+    );
 }
 
 export default App;
