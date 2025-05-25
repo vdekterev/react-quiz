8e9d6b3ecf7755d8732e06e41388e142d785e811
vdekterev <vlad.decterev@icloud.com>
2025-05-25 14:25:03 +0400

    json server

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import DateCounter from "./components/DateCounter";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import {useEffect} from "react";
 
-function App() {
+export default function App() {
+
+    useEffect(() => {
+        async function fetchQuestions() {
+            const res = await fetch("http://localhost:8000/questions");
+            const data = await res.json();
+            console.log(data);
+        }
+
+        void fetchQuestions();
+    }, []);
+
     return (
-     <div>
-         <DateCounter/>
+     <div className="app">
+        <Header />
+         <Main>
+             <p>1/15</p>
+             <p>Question?</p>
+         </Main>
      </div>
     );
 }
-
-export default App;
