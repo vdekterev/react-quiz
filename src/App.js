ec855105df2f31035120056d0f26bcd3ba468572
vdekterev <vlad.decterev@icloud.com>
2025-06-01 14:20:47 +0400

    bank account

--- src/App.js
+++ src/App.js
@@ -1,156 +1,115 @@
-import Header from "./components/Header";
-import Main from "./components/Main";
-import {useEffect, useMemo, useReducer} from "react";
-import Loader from "./components/Loader";
-import Error from "./components/Error";
-import StartScreen from "./components/StartScreen";
-import Timer from "./components/Timer";
-import Question from "./components/Question";
-import NextButton from "./components/NextButton";
-import Progress from "./components/Progress";
-import FinishScreen from "./components/FinishScreen";
+import "./index.css";
+import {useEffect, useReducer} from "react";
+import {useLocalStorage} from "./hooks/useLocalStorage";
 
-const reducer = (state, action) => {
-    // ACTION TYPE: dataReceived | dataFailed
-    // STATUS: loading | error | ready | active | finished,
+const initialState = {
+    balance: 0,
+    loan: 0,
+    isActive: false
+};
+
+function reducer(state, action) {
+    if (!state.isActive && action.type !== 'openAccount') return state;
+
+    const STARTER_BALANCE = 500;
+    const LOAN_AMOUNT = 5000;
+
     switch (action.type) {
-        case 'dataReceived':
+        /* Action types: openAccount, deposit, withdraw, requestLoan, payLoan, closeAccount */
+        case 'openAccount':
             return {
                 ...state,
-                questions: action.payload,
-                status: 'ready'
-            };
-        case 'dataFailed':
-            return {
-                ...state,
-                status: 'error'
+                balance: STARTER_BALANCE,
+                isActive: true
             }
-        case 'quizStarted':
+        case 'deposit':
             return {
                 ...state,
-                status: 'active'
+                balance: state.balance + action.payload,
             }
-        case 'quizFinished':
+        case 'withdraw':
             return {
                 ...state,
-                status: 'finished',
-                highscore: Math.max(state.highscore, state.score),
+                balance: Math.max(state.balance - action.payload, 0)
             }
-        case 'quizNextAnswer':
-            const question = state.questions[state.questionIdx];
-            const isCorrect = action.payload === question.correctOption;
+        case 'requestLoan':
+            if (state.loan) return state;
+            return {
+                ...state,
+                balance: state.balance + LOAN_AMOUNT,
+                loan: LOAN_AMOUNT
+            }
+        case 'payLoan':
+            if (!state.loan || state.balance <= 0) return state;
 
             return {
                 ...state,
-                answer: action.payload,
-                score: isCorrect ? state.score + question.points : state.score
+                balance: Math.max(state.balance - state.loan, 0),
+                loan: Math.max(state.loan - state.balance, 0)
             }
-        case 'quizNextQuestion':
+        case 'closeAccount':
+            if (state.balance || state.loan) return state;
             return {
                 ...state,
-                questionIdx: state.questionIdx + 1,
-                answer: null,
+                isActive: false
             }
-        case 'quizRestart':
-            return {
-                ...initialState,
-                status: 'ready',
-                questions: state.questions,
-                highscore: state.highscore,
-            };
         default:
-            console.error("Unknown action: ", action);
+            console.error('Unknown action: ' + action.type);
             return state;
     }
 }
 
-const initialState = {
-    questions: [],
-    status: 'loading',
-    questionIdx: 0,
-    answer: null,
-    score: 0,
-    highscore: 0,
-}
-
 export default function App() {
-    const [state, dispatch] = useReducer(reducer, initialState, undefined);
+    const [storedState] = useLocalStorage('game-state', initialState);
+
+    const [state, dispatch] = useReducer(reducer, storedState, undefined);
+
     const {
-        status,
-        questions,
-        questionIdx,
-        answer,
-        score,
-        highscore
+        balance,
+        loan,
+        isActive
     } = state;
 
-    const numQuestions = questions.length;
-
-    const maxPoints = useMemo(() => (
-        questions.reduce((acc, question) => acc + question.points, 0)
-    ), [questions])
-
     useEffect(() => {
-        fetch("http://localhost:8000/questions")
-            .then(res => res.json())
-            .then(data => dispatch({ type: 'dataReceived', payload: data }))
-            .catch(() => dispatch({ type: 'dataFailed' }))
-    }, []);
-
-    function handleNextQuestion() {
-        const type = questionIdx === numQuestions - 1 ? 'quizFinished' : 'quizNextQuestion';
-        dispatch({ type: type })
-    }
-
-    const TIME_LIMIT_SEC = questions.length * 30;
+        localStorage.setItem('game-state', JSON.stringify(state));
+    }, [state])
 
     return (
-     <div className="app">
-        <Header />
-         <Main>
-             {status === 'loading' && <Loader/>}
-             {status === 'error' && <Error/>}
-             {status === 'ready' &&
-                 <StartScreen
-                     onStart={() => dispatch({type: 'quizStarted'})}
-                     numQuestions={numQuestions}
-                 />
-             }
-             {status === 'active' &&
-                 <>
-                     <Progress
-                         idx={questionIdx}
-                         numQuestions={numQuestions}
-                         points={score}
-                         maxPoints={maxPoints}
-                         answer={answer}
-                     />
-                     <Question
-                         q={questions[questionIdx]}
-                         onAnswer={idx => dispatch({
-                             type: 'quizNextAnswer',
-                             payload: idx
-                         })}
-                         answer={answer}
-                     />
-                     <footer>
-                         <Timer timeLimit={TIME_LIMIT_SEC} onFinish={() => dispatch({type: 'quizFinished'})}/>
-                         <NextButton answer={answer} onClick={handleNextQuestion}>
-                             {questionIdx === numQuestions - 1 ? 'Finish' : 'Next'}
-                         </NextButton>
-                     </footer>
-                 </>
-             }
-             {status === 'finished' &&
-                 <FinishScreen
-                     points={score}
-                     maxPoints={maxPoints}
-                     highscore={highscore}
-                     onRestart={() => dispatch({ type: 'quizRestart' })}
-                 />
-             }
+        <div className={`App ${!isActive && "inactive"}`}>
+            <h1>useReducer Bank Account</h1>
+            <p>Balance: {balance}</p>
+            <p>Loan: {loan}</p>
 
-         </Main>
-     </div>
+            <p>
+                <button className="open-account" onClick={() => dispatch({ type: 'openAccount' })} disabled={isActive}>
+                    Open account
+                </button>
+            </p>
+            <p>
+                <button onClick={() => dispatch({ type: 'deposit', payload: 150 })} disabled={!isActive}>
+                    Deposit 150
+                </button>
+            </p>
+            <p>
+                <button onClick={() => dispatch({ type: 'withdraw', payload: 50 })} disabled={!balance}>
+                    Withdraw 50
+                </button>
+            </p>
+            <p>
+                <button onClick={() => dispatch({ type: 'requestLoan' })} disabled={!isActive || loan > 0}>
+                    Request a loan of 5000
+                </button>
+            </p>
+            <p>
+                <button onClick={() => dispatch({ type: 'payLoan' })} disabled={!isActive || !balance || !loan}>
+                    Pay loan
+                </button>
+            </p>
+            <p>
+                <button onClick={() => dispatch({ type: 'closeAccount' })} disabled={!isActive}>
+                    Close account
+                </button>
+            </p>
+        </div>
     );
 }
